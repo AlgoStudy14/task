a9e98bdf6892b635ec387a7e69f3793d8379cd8f
Proscream <proscream@naver.com>
2021-04-13 13:39:45 +0900

    dp로 풀려고 해 본 시간초과 코드

--- Task_0417/신숙/BOJ_Gold5_17090.java
+++ Task_0417/신숙/BOJ_Gold5_17090.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 
 public class BOJ_Gold5_17090 {
 	/*
+	 * <미로탈출하기>
+	 * map에 U D L R 대로 움직여서 범위 밖으로 움직이는 칸의 수를 세는 문제.
+	 * 
+	 * <풀이>
+	 * dfs, dp.
+	 * 
 	 * 시간초과.
 	 */
 	static int N, M, ans;
 	static char[][] map;
-	static boolean[][] dp, visited;
+	static int[][] dp;
+	static boolean[][] visited;
 	static boolean flag;
 	static ArrayList<int[]> arr;
 	public static void main(String[] args) {
@@ -15,13 +22,14 @@ public class BOJ_Gold5_17090 {
 		N = sc.nextInt();
 		M = sc.nextInt();
 		map = new char[N][M];
-		dp = new boolean[N][M];
+		dp = new int[N][M];
+		//입력
 		for(int i = 0; i < N; i++) {
 			String s = sc.next();
 			for(int j = 0; j < M; j++)
 				map[i][j] = s.charAt(j);
 		}
-		
+		//각 칸마다 확인하기.
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < M; j++) {
 				flag = false;
@@ -32,37 +40,45 @@ public class BOJ_Gold5_17090 {
 		}
 		System.out.println(ans);
 	}
-	
+	//int[] DP 0 : 방문하지 않은 상태. 1 방문하여 탈출성공 칸. 2 방문하여 탈출실패한 칸.
 	static void find(int r, int c) {
-		if(flag)
+		if(dp[r][c] == 1) {
+			flag = true;
+			ans++;
+			return;
+		}else if(dp[r][c] == 2)
 			return;
 		
-		if(r >= N || r < 0 || c >= M || c < 0) {
+		visited[r][c] = true;
+		arr.add(new int[] {r, c});
+		//다음 위치상태 확인하기
+		int nr, nc;
+		if(map[r][c] == 'D') {
+			nr = r + 1;
+			nc = c;
+		}else if(map[r][c] == 'U') {
+			nr = r - 1;
+			nc = c;
+		}else if(map[r][c] == 'L') {
+			nr = r;
+			nc = c - 1;
+		}else{
+			nr = r;
+			nc = c + 1;
+		}
+		
+		if(nr >= N || nr < 0 || nc >= M || nc < 0) {
 			for(int[] a : arr)
-				dp[a[0]][a[1]] = true;
+				dp[a[0]][a[1]] = 1;
 			ans++;
 			flag = true;
 			return;
 		}
-		
-		if(visited[r][c])
-			return;
-		
-		if(dp[r][c] == true) {
-			flag = true;
-			ans++;
-			return;
-		}		
-		visited[r][c] = true;
-		arr.add(new int[] {r, c});
-		
-		if(map[r][c] == 'D')
-			find(r + 1, c);
-		else if(map[r][c] == 'U')
-			find(r - 1, c);
-		else if(map[r][c] == 'L')
-			find(r, c - 1);
-		else if(map[r][c] == 'R')
-			find(r, c + 1);
+		if(!visited[nr][nc])
+			find(nr, nc);
+		if(!flag) {
+			for(int[] a : arr)
+				dp[a[0]][a[1]] = 2;
+		}
 	}
 }
