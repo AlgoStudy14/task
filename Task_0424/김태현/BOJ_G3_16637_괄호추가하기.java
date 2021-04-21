a95ef77cfdec2af3945b6f1745d036fc3c6d32ea
sskl660 <62137510+sskl660@users.noreply.github.com>
2021-04-21 23:31:20 +0900

    Update BOJ_G3_16637_괄호추가하기.java

--- Task_0424/김태현/BOJ_G3_16637_괄호추가하기.java
+++ Task_0424/김태현/BOJ_G3_16637_괄호추가하기.java
@@ -1,104 +1,140 @@
 package s0424;
 
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 /*
  * <문제 요약>
- * 구해야 하는 것 : 공익 광고가 최대한 많이 노출되는 시작 지점.
- * 문제 유형 : 시뮬레이션.
+ * 구해야 하는 것 : 괄호를 적절히 추가해 만들 수 있는 식의 최대값.
+ * 유의 조건 : 괄호를 제외한 연산자의 우선순위는 동일하다, 중첩된 괄호는 허용하지 않는다, 괄호안의 연산자는 하나만 있어야 한다.
+ * 문제 유형 : DFS, 시뮬레이션.
  * <풀이법>
- * 1. 로그를 (시 -> 분 -> 초)순으로 정렬한다.
- * 2. 로그를 {시작 시, 시작 분, 시작 초, 종료 시, 종료 분, 종료 초}순 int형 배열로 변환한다.
- * 3. 1번 로그부터 N번 로그를 시작점으로하여 광고 길이와 겹치는 부분을 계산하고 갱신한다.
- * -> 단, 광고 시간이 플레이시간을 넘어서는 경우에서 반복을 종료한다.
+ * 1. 연산자를 기준으로 괄호를 쳐보고, 치지 않아보면서 괄호를 놓는 모든 경우를 탐색한다.
+ * -> 단, 이전 연산자에 괄호가 씌워져 있다면 현재 연산자에 대한 괄호는 스킵한다.
+ * 2. 모든 괄호가 완성되었다면, 괄호 표시된(true) 연산자를 먼저 계산하고 나머지를 계산하는 시뮬레이션 진행.
+ * -> 연산자를 탐색하면서 true인 경우, 수를 계산한 후 큐에 넣는다.
+ * -> 연산자를 탐색하면서 false인 경우, 연산자를 큐에 넣는다.
+ * -> 최종적으로 큐에서 숫자를 빼면서 값을 계산하고, 최댓값을 갱신한다.
+ * <주의사항>
+ * 1. 괄호안의 연산자는 하나만 있어야 한다는 조건을 놓쳐서 다르게 풀이했었다.
+ * '중요한 조건들'은 항상 눈에 보이게 기록해두고 놓치지 말도록 하자. 풀이가 아예 다른 방향으로 흘러갈 가능성이 생긴다.
+ * 2. 괄호안의 숫자를 계산하는 로직은 연습해두면 나중에도 쓸만하다고 생각된다.
+ * 3. 전역 변수를 사용할때는 항상 주의하자. 이 문제에서 전역 변수를 일관성 없이 건드렸다가 디버깅하는데 오래 걸렸다.
  */
-public class PM_KAKAO2021_광고삽입 {
-	public String solution(String play_time, String adv_time, String[] logs) {
-		// 플레이 시간과 광고 시간이 같은 경우는 시작점 리턴
-		if (play_time.equals(adv_time)) {
-			return "00:00:00";
-		}
-		// 플레이 시간 변환
-		int play = Integer.parseInt(play_time.substring(0, 2)) * 3600 + Integer.parseInt(play_time.substring(3, 5)) * 60
-				+ Integer.parseInt(play_time.substring(6, 8));
 
-		// 광고 시간 변환
-		int adv = Integer.parseInt(adv_time.substring(0, 2)) * 3600 + Integer.parseInt(adv_time.substring(3, 5)) * 60
-				+ Integer.parseInt(adv_time.substring(6, 8));
+public class BOJ_G3_16637_괄호추가하기 {
+	static int N;
+	static int[] nums;
+	static char[] opers;
+	static boolean[] use;
+	static int max;
 
-		// 로그 시간들 정렬
-		Arrays.sort(logs);
-		System.out.println(Arrays.toString(logs));
-		// log 시간 변환
-		int[][] log = new int[logs.length][2];
-		for (int i = 0; i < logs.length; i++) {
-			log[i] = new int[] {
-					Integer.parseInt(logs[i].substring(0, 2)) * 3600 + Integer.parseInt(logs[i].substring(3, 5)) * 60
-							+ Integer.parseInt(logs[i].substring(6, 8)),
-					Integer.parseInt(logs[i].substring(9, 11)) * 3600 + Integer.parseInt(logs[i].substring(12, 14)) * 60
-							+ Integer.parseInt(logs[i].substring(15, 17)) };
-		}
-
-		// 로그 1번부터 시작점으로 하여 겹치는 시간을 계산하며 값을 갱신한다.
-		String answer = "";
-		long max = 0;
-		int start = 0;
-		for (int i = 0; i < log.length; i++) {
-			// 광고 종료 시간
-			int end = log[i][0] + adv;
-			// 총 광고 상영시간
-			long run = 0;
-
-			// 만일, 광고 종료 시간이 플레이 타임보다 크다면 반복 종료
-			if (end > play) {
-				break;
+	public static void main(String[] args) {
+		// 입력
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		nums = new int[N / 2 + 1];
+		opers = new char[N / 2];
+		use = new boolean[N / 2];
+		sc.nextLine();
+		String line = sc.nextLine();
+		int idx1 = 0, idx2 = 0;
+		for (int i = 0; i < N; i++) {
+			char cur = line.charAt(i);
+			// 숫자면 숫자 저장.
+			if (cur >= '0' && cur <= '9') {
+				nums[idx1++] = cur - '0';
 			}
+			// 연산자면 연산자 저장.
+			else {
+				opers[idx2++] = cur;
+			}
+		}
+		max = Integer.MIN_VALUE;
 
-			// 만일 로그의 시작점이 광고 종료 시간의 종료점보다 작거나 같다면
-			for (int j = 0; j < log.length; j++) {
-				if (log[j][0] < end) {
-					// 만일 종료지점도 작으면 로그 시간을 넣기
-					if (log[j][1] < end) {
-						run += (log[j][1] - log[j][0]);
-					}
-					// 그게 아니면 종료점 - 로그 시작점
-					else {
-						run += (end - log[j][0]);
-					}
+		// dfs를 이용한 연산자 괄호 치기.
+		dfs(0);
+
+		System.out.println(max);
+
+		sc.close();
+	}
+
+	private static void dfs(int cnt) {
+		// 연산자를 모두 괄호를 칠지 말지 결정해줬으면 계산 진행
+		if (cnt == use.length) {
+			count();
+			return;
+		}
+
+		// 첫 번째 연산자는 비교 대상이 없으므로 현재 연산자에 대해서 괄호를 치느냐 마느냐 두 방향으로 진행.
+		if (cnt == 0) {
+			use[cnt] = true;
+			dfs(cnt + 1);
+			use[cnt] = false;
+			dfs(cnt + 1);
+		} else {
+			// 앞 연산자에 이미 괄호를 쳤다면 현재 연산자는 무시하고 다음 dfs진행.
+			if (use[cnt - 1]) {
+				dfs(cnt + 1);
+			}
+			// 앞 연산자에 괄호가 없다면 현재 연산자에 대해서 괄호를 치느냐 마느냐 두 방향으로 진행.
+			else {
+				use[cnt] = true;
+				dfs(cnt + 1);
+				use[cnt] = false;
+				dfs(cnt + 1);
+			}
+		}
+	}
+
+	private static void count() {
+		Queue<Integer> num = new LinkedList<Integer>();
+		Queue<Character> oper = new LinkedList<Character>();
+		boolean[] visited = new boolean[N / 2 + 1];
+		for (int i = 0; i < N / 2; i++) {
+			// 현재 인덱스의 연산자가 true라면 앞 뒤의 숫자를 계산한뒤 큐에 넣는다. 숫자는 방문처리한다.
+			if (use[i]) {
+				num.offer(rule(nums[i], opers[i], nums[i + 1]));
+				visited[i] = true;
+				visited[i + 1] = true;
+			}
+			// 현재 인덱스의 연산자가 false라면 현재 숫자가 방문되어 있지 않는 경우 현재 숫자와 연산자를 각각 큐에 넣는다. 숫자는 방문처리한다.
+			else {
+				if (!visited[i]) {
+					num.offer(nums[i]);
+					visited[i] = true;
 				}
-				// 그게 아니면 반복 종료
-				else {
-					break;
-				}
-			}
-
-			// 만일, 광고 시간이 더 긴 경우에만 시작점 갱신
-			if (max < run) {
-				max = run;
-				start = log[i][0];
+				oper.offer(opers[i]);
 			}
 		}
-		// 결과 변환
-		int h = start / 3600;
-		int m = (start % 3600) / 60;
-		int s = (start % 3600) % 60;
-		if (h < 10) {
-			answer += "0" + h + ":";
-		} else {
-			answer += h + ":";
+		// 마지막 숫자가 방문처리 안되어 있다면 큐에 넣는다.
+		if (!visited[N / 2]) {
+			num.offer(nums[N / 2]);
 		}
-		if (m < 10) {
-			answer += "0" + m + ":";
-		} else {
-			answer += m + ":";
-		}
-		if (s < 10) {
-			answer += "0" + s;
-		} else {
-			answer += s;
-		}
-		System.out.println(answer);
 
-		return answer;
+		// 최종 계산
+		int ans = num.poll();
+		while (!num.isEmpty()) {
+			ans = rule(ans, oper.poll(), num.poll());
+		}
+
+		// 최댓값 갱신
+		max = Math.max(max, ans);
+	}
+
+	private static Integer rule(int i, char c, int j) {
+		switch (c) {
+		case '+':
+			return i + j;
+		case '-':
+			return i - j;
+		case '*':
+			return i * j;
+		case '/':
+			return i / j;
+		}
+		return null;
 	}
 }
