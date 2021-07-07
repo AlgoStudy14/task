3667c70f94058fb90ca3089c62f18f22be1502c8
aeeunkim <35448590+aeeunkim@users.noreply.github.com>
2021-07-07 22:01:57 +0900

    Create 1714정렬하기.java

---
+++ Task_0710/김애은/1714정렬하기.java
@@ -0,0 +1,31 @@
+package study0710;
+/*
+ * 정렬을 해서 제일 처음 두개를 더해야된다는건 문제에서 나옴
+ * 더하고 나면? 포함해서 그다음 작은 수 두개 더해야됨.
+ * 그걸 체크하기 위해 pq를 사용
+ * 사용한것: pq
+   n의 범위가 100000이기 때문에 long 사용
+ */
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main_1714정렬하기 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		long result=0;
+		int N=sc.nextInt();
+		PriorityQueue<Long> pq = new PriorityQueue<>();
+		for(int i=0;i<N;i++) {
+			pq.add(sc.nextLong());
+		}
+		while(pq.size()>1) {
+			long a=pq.poll();
+			long b=pq.poll();
+			pq.add(a+b);
+			result+=a+b;
+		}
+		System.out.println(result);
+	}
+
+}
